d51bf3e15df6660dd299629f0df547540e52803c
rajabovjavid <jvdrjbv@gmail.com>
2021-10-19 23:00:29 +0200

    populating traveler info while getting trips

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -68,8 +68,7 @@ exports.getOne = (Model, popOptions) =>
 exports.getAll = (Model) =>
   catchAsync(async (req, res, next) => {
     // To allow for nested GET reviews on tour (hack)
-    let filter = {};
-    if (req.params.tourId) filter = { tour: req.params.tourId };
+    const filter = {};
 
     const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
