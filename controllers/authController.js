3b2ea8a464d14c72bdba81778b86de5144f9e8f2
rajabovjavid <jvdrjbv@gmail.com>
2021-10-20 23:25:40 +0200

    token expire date is added to respond on login and signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,10 +14,12 @@ const signToken = (id) =>
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
+  const tokenExpireDate = new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  );
+
   res.cookie("jwt", token, {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+    expires: tokenExpireDate,
     httpOnly: true,
     secure: req.secure || req.headers["x-forwarded-proto"] === "https",
   });
@@ -28,6 +30,7 @@ const createSendToken = (user, statusCode, req, res) => {
   res.status(statusCode).json({
     status: "success",
     token,
+    tokenExpireDate,
     data: {
       user,
     },
