939ccd33792d86f951f6156eef2fccc5bfb41b24
rajabovjavid <jvdrjbv@gmail.com>
2021-10-27 08:00:38 +0200

    price calculation is set while creating trip

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,41 +1,56 @@
 const Trip = require("../models/tripModel");
+const City = require("../models/cityModel");
 
 const catchAsync = require("../utils/catchAsync");
-// const AppError = require("../utils/appError");
+const AppError = require("../utils/appError");
+const constants = require("../utils/constants");
 const factory = require("./handlerFactory");
 
-exports.createTrip = catchAsync(async (req, res) => {
-  const trip = await Trip.create({
+exports.beforeCreateTrip = catchAsync(async (req, res, next) => {
+  req.body = {
+    ...req.body,
     traveler: req.user._id,
-    origin: req.body.origin,
-    destination: req.body.destination,
+    traveler_ratings_average: req.user.traveler.ratings_average,
     pickup_deadline: new Date(req.body.pickup_deadline),
     delivery_deadline: new Date(req.body.delivery_deadline),
-    max_weight: req.body.max_weight,
     calculated_price_per_kg: req.body.calculated_price_per_kg,
     price_per_kg: req.body.calculated_price_per_kg,
-    description: req.body.description,
-  });
-
-  res.status(201).json({
-    status: "success",
-    data: {
-      data: trip,
-    },
-  });
+  };
+  next();
 });
 
 exports.calculatePricePerKg = catchAsync(async (req, res, next) => {
-  // 1) şeherler arasındakş mesafe
+  // distance
+  const originCity = await City.findOne({ city_name: req.body.origin });
+  if (!originCity) {
+    return next(new AppError("No city found with that name", 404));
+  }
+  const destinationCity = await City.findOne({
+    city_name: req.body.destination,
+  });
+  if (!destinationCity) {
+    return next(new AppError("No city found with that name", 404));
+  }
+  const { distance } = originCity.distances_to.find(
+    (item) => item.to_city_name === destinationCity.city_name
+  );
 
-  //2) çatdırılma müddeti
+  // shipping duration
   const pickup_deadline = new Date(req.body.pickup_deadline);
   const delivery_deadline = new Date(req.body.delivery_deadline);
   const diffTime = Math.abs(delivery_deadline - pickup_deadline);
   const diffInHours = Math.ceil(diffTime / (1000 * 60 * 60));
 
-  // tez çatdırılmanın qiymeti daha çox olmalıdır TODO
-  req.body.calculated_price_per_kg = diffInHours * 1;
+  // setting coefficient
+  let regionCoefficient;
+  if (req.body.region === "local")
+    regionCoefficient = constants.localCoefficient;
+  else if (req.body.region === "global")
+    regionCoefficient = constants.globalCoefficient;
+
+  // final formula
+  const calculatedValue = (distance / diffInHours) * regionCoefficient;
+  req.body.calculated_price_per_kg = Math.round(calculatedValue * 10) / 10;
 
   next();
 });
@@ -43,6 +58,8 @@ exports.calculatePricePerKg = catchAsync(async (req, res, next) => {
 exports.getTrip = factory.getOne(Trip);
 
 exports.getAllTrips = factory.getAll(Trip);
+
+exports.createTrip = factory.createOne(Trip);
 
 exports.updateTrip = factory.updateOne(Trip);
 
