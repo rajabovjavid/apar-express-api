de094c1c9d6257769d15c3bf646358a2227e7b96
rajabovjavid <jvdrjbv@gmail.com>
2021-11-01 06:11:27 +0100

    Trip and User controllers: beforeGetUser/Trip middlewares added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
-// const userController = require("../controllers/userController");
+const userController = require("../controllers/userController");
+const tripController = require("../controllers/tripController");
+const shipmentController = require("../controllers/shipmentController");
 const authController = require("../controllers/authController");
 const adminController = require("../controllers/adminController");
 // const s3Controller = require("../controllers/s3Controller");
@@ -12,12 +14,23 @@ router.use(authController.protect);
 router.use(authController.restrictTo("admin"));
 
 // user related routes
-router.route("/users").get(adminController.getAllUsers);
+router.route("/users").get(userController.getAllUsers);
 router
   .route("/users/:id")
-  .get(adminController.getUser)
-  .patch(adminController.updateUser)
-  .delete(adminController.deleteUser);
+  .get(userController.beforeGetUser, userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+// shipment related routes
+router.route("/shipments").get(shipmentController.getAllShipments);
+
+// user related routes
+router.route("/trips").get(tripController.getAllTrips);
+router
+  .route("/trips/:id")
+  .get(tripController.beforeGetTrip, tripController.getTrip)
+  .patch(tripController.updateTrip)
+  .delete(tripController.deleteTrip);
 
 // city related routes
 router
@@ -26,11 +39,5 @@ router
   .post(adminController.createCity);
 router.route("/cities/:id").get(adminController.getCity);
 router.route("/cities/:id/addDistance").patch(adminController.addDistance);
-
-// shipment related routes
-router.route("/shipments").get(adminController.getAllShipments);
-
-// user related routes
-router.route("/trips").get(adminController.getAllTrips);
 
 module.exports = router;
