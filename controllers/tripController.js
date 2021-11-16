26fede8e12bfe2c1ce396f018b083f5b3603d639
rajabovjavid <jvdrjbv@gmail.com>
2021-11-16 03:17:17 +0100

    1) tripController - districts and categories checks are added to beforeCreateTrip 2) constants - packageCategories is added

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -7,13 +7,51 @@ const constants = require("../utils/constants");
 const factory = require("./handlerFactory");
 
 exports.beforeCreateTrip = catchAsync(async (req, res, next) => {
-  req.body = {
-    ...req.body,
-    traveler: req.user._id,
-    traveler_ratings_average: req.user.traveler.ratings_average,
-    pickup_deadline: new Date(req.body.pickup_deadline),
-    delivery_deadline: new Date(req.body.delivery_deadline),
-  };
+  // origin districts check
+  if (!req.body.ori_districts?.length) {
+    // if body.ori_districts is undefined or empty
+    req.body.ori_districts = req.originCity.districts;
+  } else if (
+    // if body.ori_districts isn't subarray of originCity.districts
+    !req.body.ori_districts.every((val) =>
+      req.originCity.districts.includes(val)
+    )
+  ) {
+    return next(new AppError(`Unsupported origin district`, 404));
+  }
+
+  // destination districts check
+  if (!req.body.dest_districts?.length) {
+    // if body.dest_districts is undefined or empty
+    req.body.dest_districts = req.destinationCity.districts;
+  } else if (
+    // if body.dest_districts isn't subarray of destinationCity.districts
+    !req.body.dest_districts.every((val) =>
+      req.destinationCity.districts.includes(val)
+    )
+  ) {
+    return next(new AppError(`Unsupported destination district`, 404));
+  }
+
+  // categories check
+  if (!req.body.categories?.length) {
+    // if body.categories is undefined or empty
+    req.body.categories = constants.packageCategories;
+  } else if (
+    // if body.categories isn't subarray of constants.packageCategories
+    !req.body.categories.every((val) =>
+      constants.packageCategories.includes(val)
+    )
+  ) {
+    return next(new AppError(`Unsupported category`, 404));
+  }
+
+  // assignin remaining parts
+  req.body.traveler = req.user._id;
+  req.body.traveler_ratings_average = req.user.traveler.ratings_average;
+  req.body.pickup_deadline = new Date(req.body.pickup_deadline);
+  req.body.delivery_deadline = new Date(req.body.delivery_deadline);
+
   next();
 });
 
@@ -21,13 +59,17 @@ exports.calculatePricePerKg = catchAsync(async (req, res, next) => {
   // distance
   const originCity = await City.findOne({ city_name: req.body.origin });
   if (!originCity) {
-    return next(new AppError("No city found with that name", 404));
+    return next(
+      new AppError(`No city found with name ${req.body.origin}`, 404)
+    );
   }
   const destinationCity = await City.findOne({
     city_name: req.body.destination,
   });
   if (!destinationCity) {
-    return next(new AppError("No city found with that name", 404));
+    return next(
+      new AppError(`No city found with name ${req.body.destination}`, 404)
+    );
   }
   const { distance } = originCity.distances_to.find(
     (item) => item.to_city_name === destinationCity.city_name
@@ -53,6 +95,10 @@ exports.calculatePricePerKg = catchAsync(async (req, res, next) => {
   const calculatedValue = (distance / diffInHours) * regionCoefficient;
   req.body.calculated_price_per_kg = Math.round(calculatedValue * 10) / 10;
 
+  // assign cities to requests
+  req.originCity = originCity;
+  req.destinationCity = destinationCity;
+
   req.res_data = {
     status_code: 200,
     status: "success",
@@ -64,7 +110,7 @@ exports.calculatePricePerKg = catchAsync(async (req, res, next) => {
   next();
 });
 
-// not for admin routes
+// for public use (search trip action)
 exports.beforeGetAllTrips = (req, res, next) => {
   const pickup_deadline = {};
   pickup_deadline.gte = req.query.pickup_deadline || new Date();
