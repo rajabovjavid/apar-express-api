27990eaff69433054dd4398ab555c80a6b342667
rajabovjavid <jvdrjbv@gmail.com>
2022-08-11 14:17:04 +0200

    admin panel - basic auth

--- src/admin.js
+++ src/admin.js
@@ -1,5 +1,6 @@
+const MongoStore = require("connect-mongo");
 const AdminJS = require("adminjs");
-const AdminJSExpress = require("@adminjs/express");
+const { buildAuthenticatedRouter } = require("@adminjs/express");
 const AdminJSMongoose = require("@adminjs/mongoose");
 
 AdminJS.registerAdapter(AdminJSMongoose);
@@ -13,12 +14,33 @@ const City = require("./models/cityModel");
 
 /** @type {AdminJS.AdminJSOptions} */
 const options = {
+  branding: { companyName: "Apar Admin" },
   resources: [User, Trip, Shipment, Status, Review, City],
   rootPath: "/admin",
 };
 
 const adminJs = new AdminJS(options);
 
-const adminJsRouter = AdminJSExpress.buildRouter(adminJs);
+const adminJsRouter = buildAuthenticatedRouter(
+  adminJs,
+  {
+    cookieName: "admin-bro",
+    cookiePassword: "superlongandcomplicatedname",
+    authenticate: async (email, password) => {
+      const user = await User.findOne({ email }).select("+password");
+
+      if (user && (await user.correctPassword(password))) {
+        return user.toJSON();
+      }
+      return null;
+    },
+  },
+  null,
+  {
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
+  }
+);
 
 module.exports = { adminJs, adminJsRouter };
